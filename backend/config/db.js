06d123579cddaeb50af5c51cb7c75108aec82d9a
Sunil Kumar <sunilprakash1701@gmail.com>
2026-05-10 18:43:20 +0530

    Updated env configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://sunil:Sunilkumar1701%23@lms.m5olabk.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
 } 
--- backend/server.js
+++ backend/server.js
@@ -7,31 +7,28 @@ import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 
-
-//app config
+// app config
 const app = express()
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-//middleware
-app.use(express.json())    //when request appear it send in json format
-app.use(cors())      //asscess backend from frontend
+// middleware
+app.use(express.json())
+app.use(cors())
 
-
-//db connection
+// db connection
 connectDB();
 
-//api endpoints
-app.use("/api/food",foodRouter);
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port,()=>{
-    console.log(`Server Started on port http://localhost:${port}`)    
+app.listen(port, () => {
+    console.log(`Server Started on port ${port}`)
 })
-
